962a7351c72510955fb97c26b3917a08e3127a65
SMOO1 <bbruh188@gmail.com>
2025-06-17 13:50:04 -0400

    lalal

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GameWindow gameWindow = new GameWindow(); //create game window
-        gameWindow.setVisible(true); //show window
-    }
-}
